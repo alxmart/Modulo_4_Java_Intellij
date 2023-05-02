b9c1450d373ad1cf7424cdd4ba15a63da45acefa
alxmart <alxmartinez@gmail.com>
2023-05-02 20:04:04 -0300

    Adding tests, examples and exercises.

---
+++ Modulo_4_Java_Intellij/src/Orientacao_Objetos.java
@@ -0,0 +1,108 @@
+/*
+----------------------------
+1) Programação Linear
+----------------------------
+permite user digitar num1
+permite user digitar num2
+
+somar num1 com num2
+
+exibir o resultado da soma
+
+-----------------------------
+2) Programação Estruturada
+-----------------------------
+
+função somar
+        permite user digitar num1
+        permite user digitar num2
+
+        somar num1 com num2
+        exibir o resultado da soma
+
+Execução:
+
+1> => processo 01
+2> => função somar
+3> => processo 02
+4> => função somar
+
+** Tela Inicial do Sistema
+
+        funcao logar
+            recebe email e senha
+            verifica se o usuario existe
+            envia este usuario para a tela inicial do sistema
+
+   Botao -> logar
+
+** Tela Principal do Sistema
+
+    funcao deslogar
+        sair do sistema
+        vai para tela inicial
+
+    Botão ->  deslogar
+
+** Tela de Fornecedores
+
+funcao deslogar
+        sair do sistema
+        vai para tela inicial
+
+ deslogar
+ logar
+
+
+-----------------------------
+3) Programação Modular
+-----------------------------
+
+Módulo Usuário:
+
+     funcao logar
+          recebe email e senha
+          verifica se o usuario existe
+          envia este usuario para a tela inicial do sistema
+
+     funcao deslogar
+          sair do sistema
+          vai para tela inicial
+
+------------------------------------
+3) Programação Orientada a Objetos
+------------------------------------
+
+Classe Usuário:
+
+     fuj ncao logar
+          recebe email e senha
+          verifica se o usuario existe
+          envia este usuario para a tela inicial do sistema
+
+     funcao deslogar
+          sair do sistema
+          vai para tela inicial
+
+   => Abstração
+   => Encapsulamento
+   => Herança
+   => Polimorfismo
+
+Lower Camel Case:    jogoMarioKart
+
+Uper  Camel Case:    JogoMarioKart
+
+Snake Case:      jogo_mario_kart
+
+Kebabe Case:     jogo-mario-kart
+
+ */
+
+public class Orientacao_Objetos {
+
+    public static void main(String[] args) {
+
+
+    }
+}
